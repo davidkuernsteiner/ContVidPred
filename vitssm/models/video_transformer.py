5d2eb97837639c1ace547ee25bd7d96b8a10bbec
David Kürnsteiner <david.kuernsteiner@gmail.com>
2024-08-06 14:57:25 +0200

    update vitssm

--- vitssm/models/video_transformer.py
+++ vitssm/models/video_transformer.py
@@ -1,7 +1,11 @@
 from typing import Union, Tuple
 
+
+from einops import rearrange
 from torch import nn
+from torch import Tensor
 from timm.models.vision_transformer import VisionTransformer
+from xformers.components.positional_embedding import SinePositionalEmbedding
 
 
 class LatentNextFramePrediction(nn.Module):
@@ -18,7 +22,28 @@ class LatentNextFramePrediction(nn.Module):
         self.latent_frame_decoder = LatentFrameDecoder()
 
         self.patchify = nn.Conv2d(3, latent_dim, kernel_size=patch_size, stride=patch_size)
-        self.pos_enc = None
+        self.pos_enc = SinePositionalEmbedding()
+
+        self.frame_in_size = frame_in_size
+        self.frame_out_size = frame_out_size
+        self.patch_size = patch_size
+        self.latent_dim = latent_dim
+
+    def forward(x: Tensor):
+        """
+        Input dims: [batch, time, height, width, channel]
+        """
+        b, t, h, w, c = x.shape
+
+        x_patches = rearrange(x, "b t h w c -> (b t) c h w")
+        x_patches = self.patchify(x_patches)
+        x_patches = rearrange(x_patches, "(b t) e h w -> b (t h w) e", b=b, t=t)
+        x_patches = self.pos_enc(x_patches)
+
+        x_latent = rearrange(x, "b t h w c -> (b t) h w c")
+        x_latent = self.latent_encoder(x_latent)
+        x_latent = rearrange(x, "(b t) e -> b t e", b=b, t=t)
+        x_next_latent = self.latent_predictor(x_latent)
 
 
 class FrameEncoder(nn.Module):
@@ -45,7 +70,11 @@ class FrameEncoder(nn.Module):
             norm_layer=norm_layer,
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor):
+        """
+        Input dims: [batch * time, height, width, channel]
+        Output dims: [batch * time, embedding]
+        """
         return self.backbone(x)
 
 
@@ -54,6 +83,10 @@ class LatentPredictor(nn.Module):
         super().__init__()
 
     def forward(self, x):
+        """
+        Input dims: [batch, time, embedding]
+        Output dims: [batch, embedding]
+        """
         pass
 
 
@@ -61,5 +94,12 @@ class LatentFrameDecoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
+    def forward(self, x_latent, x_patches):
+        """
+        Input dims:
+            latent predictor: [batch, embedding]
+            frame patchify: [batch, time, height_patches, width_patches, embedding]
+
+        Output dims: [batch, time, height_out, width_out, channel]
+        """
         pass
