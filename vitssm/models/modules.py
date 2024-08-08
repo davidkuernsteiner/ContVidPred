1d6c0d6d354f469d55e6e985729d0b4cc4aeb0ac
David Kürnsteiner <david.kuernsteiner@gmail.com>
2024-08-08 13:03:57 +0200

    update models

--- vitssm/models/modules.py
+++ vitssm/models/modules.py
@@ -1,49 +1,54 @@
 from typing import Union, Tuple
-
-
+from functools import reduce
+from operator import mul
 from einops import rearrange
+from math import sqrt
+
+import torch
 from torch import nn
+import torch.nn.functional as F
 from torch import Tensor
-from timm.models.vision_transformer import VisionTransformer
-from xformers.components.positional_embedding import SinePositionalEmbedding
-from xformers.components.attention import ScaledDotProduct
-from xformers.components.feedforward import MLP
+
+from timm.layers.pos_embed import resample_abs_pos_embed
 
 
 
-class CausalTransformerBLock(nn.Module):
+class LearnablePositionalEncoding(nn.Module):
     def __init__(
-        self,
-        embedding_dim: int = 768,
-        seq_len: int = 256,
-        to_seq_len: int = 256
-    ) -> None:
+            self,
+            n_tokens: int,
+            latent_dim: int,
+            p_dropout: float = 0.
+        ):
         super().__init__()
 
-        self.norm1 = nn.LayerNorm(embedding_dim, eps=1e-6, elementwise_affine=True)
-        self.norm2 = nn.LayerNorm(embedding_dim, eps=1e-6, elementwise_affine=True)
-        self.norm3 = nn.LayerNorm(embedding_dim, eps=1e-6, elementwise_affine=True)
+        self.pos_enc = nn.Parameter(torch.randn(1, n_tokens, latent_dim) * .02)
+        self.dropout = nn.Dropout(p=p_dropout)
 
-        self.q_proj = nn.Linear(embedding_dim, embedding_dim)
-        self.k_proj = nn.Linear(embedding_dim, embedding_dim)
-        self.v_proj = nn.Linear(embedding_dim, embedding_dim)
-        self.out_proj = nn.Linear(embedding_dim, embedding_dim)
+        self.n_tokens = n_tokens
+        self.latent_dim = latent_dim
 
-        self.attn = ScaledDotProduct(dropout=0.1, causal=True)
-        self.mlp = MLP(embedding_dim, embedding_dim)
+    def forward(self, x: torch.Tensor):
+        """
+        Input dims: [batch, token, latent]
+        Output dims: [batch, token, latent]
+        """
+        _, t, _ = x.shape
+        pos_enc = self.resample_pos_enc(t)
 
-    def forward(self, x: Tensor) -> Tensor:
-        x += self.attn(x, x, x)
+        return self.dropout(x + pos_enc)
 
-        x = self.ln1(x)
+    def resample_pos_enc(self, new_n_tokens: int):
+        new_hw = int(sqrt(new_n_tokens))
+        new_size = new_hw, new_hw
+        old_hw = int(sqrt(self.pos_enc.shape[1]))
 
-        q = self.q_proj(x)
-        k = self.k_proj(x)
-        v = self.v_proj(x)
+        orig_dtype = self.pos_enc.dtype
+        pos_enc = self.pos_enc.float()  # interpolate needs float32
+        pos_enc = rearrange(pos_enc, "b (h w) e -> b e h w", h=old_hw, w=old_hw)
+        pos_enc = F.interpolate(pos_enc, size=new_size, mode="bicubic", antialias=True)
+        pos_enc = rearrange(pos_enc, "b e h w -> b (h w) e")
+        pos_enc = pos_enc.to(orig_dtype)
 
-        x = self.attn(q, k, v)
-        x = self.out_proj(x)
-        x = self.ln2
-        out = self.norm(out)
-
-        return out
+        return pos_enc
+    
--- vitssm/models/next_frame_prediction.py
+++ vitssm/models/next_frame_prediction.py
@@ -10,16 +10,20 @@ from xformers.components.positional_embedding import SinePositionalEmbedding
 from xformers.components.attention import ScaledDotProduct, LocalAttention
 from xformers.components.multi_head_dispatch import MultiHeadDispatch
 
+from .modules import LearnablePositionalEncoding
+
 
 class LatentNextFramePrediction(nn.Module):
     def __init__(
         self,
-        frame_in_size: Union[int, Tuple[int, int]] = 224,
-        frame_out_size: Union[int, Tuple[int, int]] = 224,
-        n_frames: int = 10,
+        frame_in_size: Tuple[int, int] = (224, 224),
+        frame_out_size: Tuple[int, int] = (224, 224),
+        n_frames: int = 30,
+        window_size: int = 10,
         patch_size: int = 16,
         latent_dim: int = 768,
         num_heads: int = 8,
+        pos_enc_dropout: float = 0.
     ):
         super().__init__()
         self.frame_encoder = FrameEncoder(
@@ -30,15 +34,17 @@ class LatentNextFramePrediction(nn.Module):
         self.latent_predictor = LatentPredictor(
             latent_dim=latent_dim,
             num_heads=num_heads,
-            window_size=n_frames,
+            window_size=window_size,
         )
         self.latent_frame_decoder = LatentFrameDecoder()
 
         self.patchify = nn.Conv2d(3, latent_dim, kernel_size=patch_size, stride=patch_size)
-        self.pos_enc = nn.Parameter(torch.randn(1, (frame_in_size // patch_size)**2, latent_dim) * .02)
+        self.pos_enc_patches = LearnablePositionalEncoding(tuple(x // patch_size for x in frame_in_size), latent_dim, p_dropout=pos_enc_dropout)
 
         self.frame_in_size = frame_in_size
         self.frame_out_size = frame_out_size
+        self.n_frames = n_frames
+        self.window_size = window_size
         self.patch_size = patch_size
         self.latent_dim = latent_dim
 
@@ -51,13 +57,14 @@ class LatentNextFramePrediction(nn.Module):
 
         x_patches = rearrange(x, "b t h w c -> (b t) c h w")
         x_patches = self.patchify(x_patches)
-        x_patches = rearrange(x_patches, "(b t) e h w -> b (t h w) e", b=b, t=t)
+        x_patches = rearrange(x_patches, "(b t) e h w -> (b t) (h w) e")
         x_patches = self.pos_enc(x_patches)
+        x_patches = rearrange(x_patches, "(b t) (h w) e -> b t (h w) e", b=b, t=t)
 
         x_latent = rearrange(x, "b t h w c -> (b t) c h w")
         x_latent = self.frame_encoder(x_latent)
         x_latent = rearrange(x_latent, "(b t) e -> b t e", b=b, t=t)
-        x_latent = self.pos_enc(x_latent)
+        #x_latent = self.pos_enc(x_latent)
         x_next_latent = self.latent_predictor(x_latent)
 
         #canvas = self.pos_enc(torch.ones(b, t, *self.frame_out_size, c))
@@ -95,7 +102,7 @@ class FrameEncoder(nn.Module):
 
     def forward(self, x: Tensor):
         """
-        Input dims: [batch * time, height, width, channel]
+        Input dims: [batch * time, channel, height, width]
         Output dims: [batch * time, latent]
         """
         return self.backbone(x)
@@ -106,25 +113,36 @@ class LatentPredictor(nn.Module):
         self,
         latent_dim: int = 768,
         num_heads: int = 8,
-        window_size: int = 5,
+        num_blocks: int = 4,
+        n_frames: int = 30,
+        window_size: int = 10,
     ) -> None:
         super().__init__()
-        att = LocalAttention(causal=True, window_size=window_size)
-        self.att_block = MultiHeadDispatch(latent_dim, num_heads, att)
+        attention_mechanism = LocalAttention(causal=True, window_size=window_size)
 
-    def forward(self, x):
+        self.pos_enc = LearnablePositionalEncoding()
+        self.blocks = nn.Sequential(
+            *(MultiHeadDispatch(latent_dim, num_heads, attention_mechanism) for _ in range(num_blocks))
+        )
+
+    def forward(self, x: torch.Tensor):
         """
         Input dims: [batch, time, latent]
         Output dims: [batch, time, latent]
         """
-        return self.att_block(x)
+        return self.blocks(self.pos_enc(x))
 
 
 class LatentFrameDecoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x_latent, x_patches, canvas):
+    def forward(
+            self,
+            x_latent: torch.Tensor,
+            x_patches: torch.Tensor, 
+            canvas: torch.Tensor,
+        ):
         """
         Input dims:
             latent predictor: [batch, latent]
