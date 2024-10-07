fix ema model averaging and introduce latte diffusion model

vitssm/engine/__init__.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import gc
 import os
 from datetime import datetime
@@ -52,9 +53,10 @@ class ModelEngine:
             self.ema = AveragedModel(
                 self.model,
                 device=self.device,
-                avg_fn=get_ema_multi_avg_fn(0.999),
+                multi_avg_fn=get_ema_multi_avg_fn(0.999),
+                use_buffers=True
             )
-            self.eval_model = self.ema
+            self.eval_model = self.ema.module
         else:
             self.eval_model = self.model
 
@@ -75,29 +77,37 @@ class ModelEngine:
 
             self.model.train()
             for x, y in tqdm(train_dataloader, total=len(train_dataloader), desc=f"Epoch {self.state["epoch"]}"):
-                loss, outs = self._train_step(x.to(self.device), y.to(self.device))
+                loss, train_outs = self._train_step(x.to(self.device), y.to(self.device))
                 self.state["step"] += 1
+                
                 if self.use_ema and (self.state["step"] % self.ema_steps == 0):
                     self.ema.update_parameters(self.model)
 
-                if self.state["step"] == self.config.optimization.get("training_steps", 10000):
+                if self.state["step"] == self.config.optimization.get("steps", torch.inf):
                     done = True
                     train_metrics = {"loss": loss}
                     self._log_train(self.state["epoch"], self.state["step"], train_metrics)
                     break
+                elif self.state["epoch"] == self.config.optimization.get("epochs", torch.inf):
+                    done = True
 
                 if self.state["step"] % self.config.get("log_freq", 0) == 0:
-                    train_metrics = {"loss": loss, "learning_rate": self.scheduler.get_last_lr()[-1]} | outs
+                    train_metrics = {"loss": loss, "learning_rate": self.scheduler.get_last_lr()[-1]} | train_outs
                     self._log_train(self.state["epoch"], self.state["step"], train_metrics)
 
             if self.use_ema:
                 update_bn(train_dataloader, self.ema, device=self.device)
                 
+            
+            eval_metrics = defaultdict(list)
             self.eval_model.eval()
             for x, y in eval_dataloader:
-                eval_loss, _ = self._eval_step(x.to(self.device), y.to(self.device))
-
-            self._log_eval(self.state["epoch"], self.state["step"], self.metrics.compute() if self.metrics is not None else {"loss": eval_loss})
+                eval_outs = self._eval_step(x.to(self.device), y.to(self.device))
+                for k, v in eval_outs.items():
+                    eval_metrics[k].append(v)
+                
+            eval_metrics = {k: sum(v) / len(v) for k, v in eval_metrics.items() if v}
+            self._log_eval(self.state["epoch"], self.state["step"], self.metrics.compute() | eval_metrics if self.metrics is not None else eval_metrics)
 
             if (self.scheduler is not None) and (not self.scheduler_step_on_batch):
                 self.scheduler.step()
@@ -121,7 +131,7 @@ class ModelEngine:
     def _train_step(self, _x: Tensor, _y: Tensor) -> tuple[float, dict[str, Any]]:
         raise NotImplementedError
 
-    def _eval_step(self, _x: Tensor, _y: Tensor) -> tuple[float, Tensor]:
+    def _eval_step(self, _x: Tensor, _y: Tensor) -> dict[str, float]:
         raise NotImplementedError
 
     @staticmethod

vitssm/engine/tasks.py
@@ -17,13 +17,13 @@ class ActionRecognitionEngine(ModelEngine):
         super().__init__(model, run_object)
 
     def _train_step(self, _x: Tensor, _y: Tensor) -> tuple[float, dict[str, float]]:
+        self.optimizer.zero_grad()
         with torch.autocast(device_type=self.device.type, dtype=torch.bfloat16, enabled=self.use_amp):
             _pred = self.model(_x)
             _loss = self.criterion(_pred, _y)
         self.scaler.scale(_loss).backward()
         self.scaler.step(self.optimizer)
         self.scaler.update()
-        self.optimizer.zero_grad()
 
         if (self.scheduler is not None) and self.scheduler_step_on_batch:
             self.scheduler.step()
@@ -47,13 +47,13 @@ class NextFrameEngine(ModelEngine):
         super().__init__(model, run_object)
 
     def _train_step(self, _x: Tensor, _y: Tensor) -> tuple[float, dict[str, float]]:
+        self.optimizer.zero_grad()
         with torch.autocast(device_type=self.device.type, dtype=torch.bfloat16, enabled=self.use_amp):
             _pred = self.model(_x)
             _loss = self.criterion(_pred, _y)
         self.scaler.scale(_loss).backward()
         self.scaler.step(self.optimizer)
         self.scaler.update()
-        self.optimizer.zero_grad()
 
         if (self.scheduler is not None) and self.scheduler_step_on_batch:
             self.scheduler.step()
@@ -77,6 +77,7 @@ class VideoVAEEngine(ModelEngine):
         super().__init__(model, run_object)
     
     def _train_step(self, _x: Tensor, _y: Tensor) -> tuple[float, dict[str, float]]:
+        self.optimizer.zero_grad()
         with torch.autocast(device_type=self.device.type, dtype=torch.bfloat16, enabled=self.use_amp):
             _x = rearrange(_x, "b t c h w -> (b t) c h w")
             _posterior = self.model.encode(_x).latent_dist
@@ -90,7 +91,6 @@ class VideoVAEEngine(ModelEngine):
         self.scaler.scale(_loss).backward()
         self.scaler.step(self.optimizer)
         self.scaler.update()
-        self.optimizer.zero_grad()
         
         if (self.scheduler is not None) and self.scheduler_step_on_batch:
             self.scheduler.step()
@@ -98,14 +98,18 @@ class VideoVAEEngine(ModelEngine):
         return _loss.item(), {"recon_loss": _recon_loss.item(), "kl_loss": _kl_loss.item()}
     
     @torch.no_grad()
-    def _eval_step(self, _x: Tensor, _y: Tensor) -> tuple[float, Tensor]:
+    def _eval_step(self, _x: Tensor, _y: Tensor) -> dict[str, float]:
         with torch.autocast(device_type=self.device.type, dtype=torch.bfloat16, enabled=self.use_amp):
             _x = rearrange(_x, "b t c h w -> (b t) c h w")
             _posterior = self.eval_model.encode(_x).latent_dist
-            _recon = self.eval_model.decode(_posterior.mode()).sample  
-            _loss = self.criterion(_recon, _x) + _posterior.kl().mean()
+            _recon = self.eval_model.decode(_posterior.mode()).sample
+            
+            beta = min(1.0, self.state["epoch"] / self.config.optimization.epochs)
+            _recon_loss = self.criterion(_recon, _x)
+            _kl_loss = _posterior.kl().mean()
+            _loss = _recon_loss + beta * _kl_loss
         
         if self.metrics is not None:
             self.metrics.update(_recon, _x)
         
-        return _loss.item(), _recon
+        return {"loss": _loss.item(), "recon_loss": _recon_loss.item(), "kl_loss": _kl_loss.item()}